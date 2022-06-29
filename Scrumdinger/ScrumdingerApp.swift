3dbfa31a6de2856de35185d5d8cf0d076cb8a192
Brodie Friday <brodieleefriday@gmail.com>
2022-06-29 11:44:00 -0500

    Creating a Card View
    added DailyScrum.swfit, Theme.swift, TrailingIconLabelStyle.swift, App.swift, CardView.swift

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Brodie Friday on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
